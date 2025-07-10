fc7ba0223d173fbc9c5fcf33811d809d6a78331a
Mushegh Andrasyan <mushegh.andrasyan@gmail.com>
2025-07-10 17:53:31 +0400

    adds swiper

--- src/main.js
+++ src/main.js
@@ -114,3 +114,18 @@ tooltipTriggers.forEach((trigger) => {
     openPopup(name, locations);
   });
 });
+
+new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  initialSlide: 4,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
